bb7626c0a5b5b72e5907de9dbb09584542675f53
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-09-30 23:46:36 +0200

    Skeleton
    Found, switched and prepared some of the assets, created a custom class and laid out the first chunks of interface.

---
+++ Consolidation_7-Shooting_Gallery_Game/DeadDuckWalking.swift
@@ -0,0 +1,13 @@
+//
+//  DeadDuckWalking.swift
+//  Consolidation_7-Shooting_Gallery_Game
+//
+//  Created by Edwin Przeźwiecki Jr. on 30/09/2022.
+//
+
+import UIKit
+import SpriteKit
+
+class DeadDuckWalking: SKNode {
+    
+}
--- Consolidation_7-Shooting_Gallery_Game/GameScene.swift
+++ Consolidation_7-Shooting_Gallery_Game/GameScene.swift
@@ -6,83 +6,69 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ducks = [DeadDuckWalking]()
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var gameTime: Timer?
+    var gameOverLabel: SKLabelNode!
+    var restartLabel: SKLabelNode!
     
     override func didMove(to view: SKView) {
+        backgroundColor = .white
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let firstRow = SKSpriteNode(imageNamed: "water-bg")
+        firstRow.zPosition = 0
+        firstRow.position = CGPoint(x: 512, y: 128)
+        firstRow.size.width = view.frame.size.width
+        addChild(firstRow)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let secondRow = SKSpriteNode(imageNamed: "water-fg")
+        secondRow.zPosition = -1
+        secondRow.position = CGPoint(x: 512, y: 256)
+        secondRow.size.width = view.frame.size.width
+        addChild(secondRow)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        let thirdRow = SKSpriteNode(imageNamed: "water-bg")
+        thirdRow.zPosition = -2
+        thirdRow.position = CGPoint(x: 512, y: 384)
+        thirdRow.size.width = view.frame.size.width
+        addChild(thirdRow)
+        
+        let firstBullets = SKSpriteNode(imageNamed: "shots3")
+        firstBullets.zPosition = -2
+        firstBullets.position = CGPoint(x: 974, y: 738)
+        addChild(firstBullets)
+        
+        let lastBullets = SKSpriteNode(imageNamed: "shots3")
+        lastBullets.zPosition = -2
+        lastBullets.position = CGPoint(x: 893, y: 738)
+        addChild(lastBullets)
+        
+        scoreLabel = SKLabelNode(fontNamed: "GillSans-UltraBold")
+        scoreLabel.zPosition = 1
+        scoreLabel.position = CGPoint(x: 12, y: 728)
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.text = "Score: 0"
+        scoreLabel.fontColor = .gray
+        addChild(scoreLabel)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
     }
 }
